c9dad2abdb5a77f0aabf821b356a9c69b91f7e59
ALIA DRAME <aliam.drame@gmail.com>
2023-05-14 05:27:32 +0200

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="footer mt-auto py-3 bg-dark">
       <div className="container">
         <span className="text-muted">
-          <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="text-decoration-none text-light">GitHub</a> | <a href="https://gnosis-safe.io/docs/" target="_blank" rel="noopener noreferrer" className="text-decoration-none text-light">Documentation Gnosis Safe</a>
+          <a href="https://github.com/dramob/Ethglobal-lisbon-SafeStack" target="_blank" rel="noopener noreferrer" className="text-decoration-none text-light">GitHub</a> | <a href="https://gnosis-safe.io/docs/" target="_blank" rel="noopener noreferrer" className="text-decoration-none text-light">Documentation Gnosis Safe</a>
         </span> <span className="text-muted">© 2023 SafeStack. All rights reserved. | </span>
         <a
           href="https://www.linkedin.com/company/kryptosphere/"
