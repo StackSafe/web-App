590cbc824888c0369a0efd0d85d32a17e5cd8995
ALIA DRAME <aliam.drame@gmail.com>
2023-05-14 06:14:05 +0200

    site carré

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,7 @@
 // Header.js
 import React, { useState, useCallback } from 'react';
 import styles from './Header.css';
+
 const Header = () => {
   const [defaultAccount, setDefaultAccount] = useState('null');
   const [connButtonText, setConnButtonText] = useState('Connect Wallet');
@@ -24,10 +25,11 @@ const Header = () => {
       console.error(error);
     }
   });
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-black">
       <div className="container">
-        <a className="{styles.logo}" href="#">
+        <a className={styles.logo} href="#">
           <img src="/logo1.png" alt="Logo" width="90" height="90" className="d-inline-block align-text-top me-2" />
         </a>
         <div className="header-content d-flex">
@@ -35,12 +37,11 @@ const Header = () => {
             <h1 className="navbar-title mx-auto">SafeStack</h1>
           </div>
           <div className="buttons-container d-flex">
-          <a className="btn btn-outline-light me-2" href="#about">About</a>
-<a className="btn btn-outline-light me-2" href="#team">Our Team</a>
-<a className="btn btn-outline-light" href="#dcapp">DCApp</a>
+            <a className="btn btn-outline-light me-2" href="#about">About</a>
+            <a className="btn btn-outline-light me-2" href="#team">Our Team</a>
+            <a className="btn btn-outline-light me-2" href="#dcapp">DCApp</a>
             <button className="btn btn-outline-light me-2" onClick={connectMetaMask}>{connButtonText}</button>
-            <button className="btn btn-outline-light" onClick={disconnect}>disconnect</button>
-          
+            <button className="btn btn-outline-light" onClick={disconnect}>Disconnect</button>
           </div>
         </div>
       </div>
@@ -49,9 +50,6 @@ const Header = () => {
 };
 
 export default Header;
-  
-
-
 
 
 
